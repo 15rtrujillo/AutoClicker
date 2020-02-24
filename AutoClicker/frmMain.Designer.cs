a18d46115b77a298f054530e91d55341d54a0e00
15rtrujillo <ryantrujillo@comcast.net>
2020-02-23 21:43:55 -0700

    Initial commit

---
+++ AutoClicker/frmMain.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace AutoClicker
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.numInterval = new System.Windows.Forms.NumericUpDown();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.btnStop = new System.Windows.Forms.Button();
+			this.tmrClick = new System.Windows.Forms.Timer(this.components);
+			this.lblInterval = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.numInterval)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// numInterval
+			// 
+			this.numInterval.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.numInterval.Location = new System.Drawing.Point(72, 12);
+			this.numInterval.Maximum = new decimal(new int[] {
+            120000,
+            0,
+            0,
+            0});
+			this.numInterval.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			this.numInterval.Name = "numInterval";
+			this.numInterval.Size = new System.Drawing.Size(120, 22);
+			this.numInterval.TabIndex = 0;
+			this.numInterval.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			this.numInterval.ValueChanged += new System.EventHandler(this.numInterval_ValueChanged);
+			// 
+			// btnStart
+			// 
+			this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnStart.Location = new System.Drawing.Point(15, 40);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(75, 25);
+			this.btnStart.TabIndex = 1;
+			this.btnStart.Text = "Start";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+			// 
+			// btnStop
+			// 
+			this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnStop.Location = new System.Drawing.Point(117, 40);
+			this.btnStop.Name = "btnStop";
+			this.btnStop.Size = new System.Drawing.Size(75, 25);
+			this.btnStop.TabIndex = 2;
+			this.btnStop.Text = "Stop";
+			this.btnStop.UseVisualStyleBackColor = true;
+			this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+			// 
+			// tmrClick
+			// 
+			this.tmrClick.Interval = 1000;
+			this.tmrClick.Tick += new System.EventHandler(this.tmrClick_Tick);
+			// 
+			// lblInterval
+			// 
+			this.lblInterval.AutoSize = true;
+			this.lblInterval.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblInterval.Location = new System.Drawing.Point(12, 14);
+			this.lblInterval.Name = "lblInterval";
+			this.lblInterval.Size = new System.Drawing.Size(54, 16);
+			this.lblInterval.TabIndex = 3;
+			this.lblInterval.Text = "Interval:";
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(207, 73);
+			this.Controls.Add(this.lblInterval);
+			this.Controls.Add(this.btnStop);
+			this.Controls.Add(this.btnStart);
+			this.Controls.Add(this.numInterval);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.MaximizeBox = false;
+			this.Name = "frmMain";
+			this.Text = "AutoClicker";
+			this.TopMost = true;
+			((System.ComponentModel.ISupportInitialize)(this.numInterval)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NumericUpDown numInterval;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.Button btnStop;
+		private System.Windows.Forms.Timer tmrClick;
+		private System.Windows.Forms.Label lblInterval;
+	}
+}
+
---
+++ AutoClicker/frmMain.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace AutoClicker
+{
+	public partial class frmMain : Form
+	{
+		[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+		public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+		//Mouse actions
+		private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+		private const int MOUSEEVENTF_LEFTUP = 0x04;
+		private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+		private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			tmrClick.Enabled = true;
+		}
+
+		public void DoMouseClick()
+		{
+			//Call the imported function with the cursor's current position
+			uint X = (uint)Cursor.Position.X;
+			uint Y = (uint)Cursor.Position.Y;
+			mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+		}
+
+		private void btnStop_Click(object sender, EventArgs e)
+		{
+			tmrClick.Enabled = false;
+		}
+
+		private void tmrClick_Tick(object sender, EventArgs e)
+		{
+			DoMouseClick();
+		}
+
+		private void numInterval_ValueChanged(object sender, EventArgs e)
+		{
+			tmrClick.Interval = Convert.ToInt32(numInterval.Value);
+		}
+	}
+}
